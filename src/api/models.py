1ed3232e7a170d371a14ebd57df34bbc91bc5cc6
akers2534 <akers2534@gmail.com>
2023-03-14 00:12:52 +0000

    3/13/23

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -15,5 +16,42 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+        }
+
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(250), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default= func.now())
+    author = db.Column(db.Integer,db.Foreignkey('parks.id'), nullable=False)
+    parks.id = db.Column(db.Integer, db.Foreignkey('parks.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Comment {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "date_created": self.date_created,
+            "author": self.author
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(250), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default= func.now())
+    author = db.Column(db.Integer,db.Foreignkey('parks.id'), nullable=False)
+    parks.id = db.Column(db.Integer, db.Foreignkey('parks.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Review {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "date_created": self.date_created,
+            "author": self.author
             # do not serialize the password, its a security breach
         }
