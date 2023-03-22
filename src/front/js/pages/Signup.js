1584314ef21a83785e8ee207f6469cee5f4f9e42
akers2534 <akers2534@gmail.com>
2023-03-22 00:51:18 +0000

    almost there

--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
-export const Login = () => {
+export const Signup = () => {
   const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
   function handel_submit(e) {
@@ -24,41 +23,11 @@ export const Login = () => {
           </label>
         </div>
 
-        <div class="row mb-4">
-          <div class="col d-flex justify-content-center">
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="form2Example31"
-                checked
-              />
-              <label class="form-check-label" for="form2Example31">
-                {" "}
-                Remember me{" "}
-              </label>
-            </div>
-          </div>
-
-          <div class="col">
-            <a href="#!">Forgot password?</a>
-          </div>
-        </div>
-
         <button type="button" class="btn btn-primary btn-block mb-4">
-          Sign in
+          Sign Up!
         </button>
 
         <div class="text-center">
-          <p>Not a Bark Pal? </p>
-          <span>
-            <p>or sign up with:</p>{" "}
-            <Link to="/Signup">
-              <a href="#!">Sign Up!</a>
-            </Link>
-          </span>
-          <br></br>
           <button type="button" class="btn btn-link btn-floating mx-1">
             <i class="fab fa-facebook-f"></i>
           </button>
